d9ac5b49edef966aa3e4b47ff4deb3a216aff9b5
nickmura <nickmuradev@gmail.com>
2025-04-21 13:14:59 -0600

    replaced my links & added my sites

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,16 @@
 <section class="w-screen p-12">
-	<h1 class="text-green-300 mb-0 uppercase font-extrabold text-3xl lg:text-4xl font-secondary">
-		Decentralised
+	<h1 class="text-indigo-300 mb-0 uppercase font-extrabold text-3xl lg:text-4xl font-secondary">
+		Nick Mura
 	</h1>
-	<h1 class="mt-0 uppercase font-secondary font-extrabold text-3xl lg:text-4xl">Tech</h1>
 	<p class="mt-8 font-primary font-medium text-3xl bg-clip-text text-white">
-		Kiwi developer building cool things.
+		Software developer/creator building cool shit from Canada
 	</p>
 </section>
 <section
 	class="w-screen p-12 flex flex-col md:flex-row justify-center items-center gap-4 flex-wrap"
 >
 	<a
-		href="https://twitter.com/0xjimmy_eth"
+		href="https://twitter.com/nickmura2"
 		class="fill-white w-16 transition duration-300 hover:fill-green-100"
 	>
 		<svg class="w-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
@@ -21,7 +20,7 @@
 		></a
 	>
 	<a
-		href="https://github.com/DecentralisedTech"
+		href="https://github.com/nickmura"
 		class="fill-white w-16 transition duration-300 hover:fill-green-100"
 	>
 		<svg class="w-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"
@@ -33,81 +32,44 @@
 </section>
 <section class="w-screen p-12 flex flex-col gap-4 items-center">
 	<a
-		href="https://assetmerge.org"
+		href="https://stream.gift"
 		target="_blank"
 		class="h-max w-full md:max-w-[80%] border border-[rgba(255,255,255,0.125)] rounded-xl flex gap-4 p-4 items-center justify-start"
 	>
-		<svg
-			class="w-20 px-2 m-2 fill-white inline-block"
-			viewBox="0 0 70.316162 74.742897"
-			version="1.1"
-			id="svg5"
-			xmlns:xlink="http://www.w3.org/1999/xlink"
-			xmlns="http://www.w3.org/2000/svg"
-		>
-			<g id="layer1" style="display:inline" transform="translate(-2.9693716,-3.0932875)">
-				<path
-					style="stroke-width:0.398482"
-					d="m 2.9693716,76.149595 6.42509,-7.43648 c 1.9520704,-2.259356 6.0548304,-6.60922 8.9827704,-9.78577 2.92793,-3.17656 6.09157,-6.62563 7.03031,-7.66461 0.93874,-1.03897 2.3725,-2.59403 3.18614,-3.45568 0.81364,-0.86164 2.56391,-2.77612 3.8895,-4.25438 1.32559,-1.47826 3.17219,-3.49878 4.10356,-4.49003 0.93136,-0.99126 2.43777,-2.62374 3.34758,-3.62774 0.9098,-1.004 2.31676,-2.52555 3.12657,-3.38123 1.85229,-1.95722 10.03913,-10.89785 11.96792,-13.06987 0.79608,-0.89646 3.91484,-4.702763 7.37753,-8.01838 l 8.22131,-7.8721375 c 0.91107,0.654216 1.57735,1.21956 2.64052,1.843397 0.36554,0.40894 -5.02933,5.7588205 -17.32547,19.0957605 -8.57716,9.30316 -19.03762,20.67503 -23.24547,25.27082 -4.20785,4.59579 -7.92978,8.62475 -8.27096,8.95324 -0.34117,0.32849 -2.10909,2.26178 -3.9287,4.29619 -8.24511,9.21845 -13.5461504,15.281569 -14.0735104,15.283487 -2.1403,-0.926851 -0.69377,-0.09166 -3.45469,-1.686587 z M 31.870142,61.886805 c 1.22773,-1.350508 2.01169,-2.260886 3.21283,-3.5515 h 12.24744 12.24743 v -13.21229 -13.2123 c 1.09965,-1.202262 2.36066,-2.744131 3.70301,-4.08973 0.29848,-0.17127 0.43247,5.11211 0.43247,17.0529 v 17.30106 h -15.92159 c -8.75688,0 -15.92159,-0.12966 -15.92159,-0.28814 z M 8.5601516,51.903895 c -1.98431,-4.52226 -2.45857,-6.83686 -2.45857,-11.99888 0,-8.77543 2.85138,-15.236 9.4134104,-21.3286 4.45221,-4.13373 9.11521,-6.60844 15.32128,-8.13119 4.85257,-1.1906605 16.86024,-0.5394405 18.52322,1.00457 0.27605,0.2563 0.0151,0.83513 -0.78375,1.7387 -0.93248,1.05468 -1.42641,1.31496 -2.23143,1.17591 -8.15751,-1.40911 -13.74286,-0.94543 -19.81146,1.64466 -7.62255,3.25332 -13.80487,10.21727 -15.75999,17.75252 -1.0927804,4.2117 -0.53662,12.39901 1.04331,15.35857 1.14081,2.13699 1.1152,2.57175 -0.2356,4.00017 -0.71533,0.75643 -1.43168,1.37533 -1.5919004,1.37533 -0.16021,0 -0.80304,-1.16629 -1.42852,-2.59176 z"
-					id="path3152"
-				/>
-			</g>
-		</svg>
+		<img src='./streamgift.png' class='w-20 rounded-xl' alt=''>
 		<div class="w-full flex flex-col p-4">
-			<h2 class="text-2xl font-bold">AssetMerge</h2>
-			<h3 class="text-xl text-gray-500">Liquidity pools for non-fungible and fungible assets.</h3>
+			<h2 class="text-2xl font-bold">stream.gift</h2>
+			<h3 class="text-xl text-gray-500">send crypto donations to livestreamers</h3>
 		</div>
 	</a>
+
 	<a
-		href="https://www.npmjs.com/package/ether-state"
-		target="_blank"
-		class="h-max w-full md:max-w-[80%] border border-[rgba(255,255,255,0.125)] rounded-xl flex gap-4 p-4 items-center justify-start"
-	>
-		<svg class="w-20" viewBox="0 0 780 250"
-			><path
-				fill="white"
-				d="M240,250h100v-50h100V0H240V250z M340,50h50v100h-50V50z M480,0v200h100V50h50v150h50V50h50v150h50V0H480z M0,200h100V50h50v150h50V0H0V200z"
-				style="--darkreader-inline-fill: #191b1c;"
-				data-darkreader-inline-fill=""
-			/></svg
-		>
-		<div class="w-full flex flex-col p-4">
-			<h2 class="text-2xl font-bold">ether-state</h2>
-			<h3 class="text-xl text-gray-500">Library for efficently syncing state from contracts.</h3>
-		</div>
+	href="https://awpswap.io"
+	target="_blank"
+	class="h-max w-full md:max-w-[80%] border border-[rgba(255,255,255,0.125)] rounded-xl flex gap-4 p-4 items-center justify-start"
+>
+	<img src='./awpswap-greyscale.png' class='w-20 rounded-xl' alt=''>
+	<div class="w-full flex flex-col p-4">
+		<h2 class="text-2xl font-bold">AWPSwap.io</h2>
+		<h3 class="text-xl text-gray-500">
+			swap cs2 skins for crypto instantly
+		</h3>
+	</div>
 	</a>
+
+
 	<a
-		href="https://github.com/DecentralisedTech/svelte-kit-ethers-template"
+		href="https://receive.me"
 		target="_blank"
 		class="h-max w-full md:max-w-[80%] border border-[rgba(255,255,255,0.125)] rounded-xl flex gap-4 p-4 items-center justify-start"
 	>
-		<svg class="w-20 fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"
-			><path
-				d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
-			/></svg
-		>
+		<p class='text-5xl w-20 pl-6'>r</p>
+
 		<div class="w-full flex flex-col p-4">
-			<h2 class="text-2xl font-bold">svelte-kit-ethers-template</h2>
-			<h3 class="text-xl text-gray-500">
-				Template for building Ethereum dApps with Svelte Kit, TailwindCSS & Ethers.
-			</h3>
+			<h2 class="text-2xl font-bold">receive.me</h2>
+			<h3 class="text-xl text-gray-500">web3 address/link profiles</h3>
 		</div>
 	</a>
-	<a
-		href="https://github.com/DecentralisedTech/P2PTrade"
-		target="_blank"
-		class="h-max w-full md:max-w-[80%] border border-[rgba(255,255,255,0.125)] rounded-xl flex gap-4 p-4 items-center justify-start"
-	>
-		<svg class="w-20 fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"
-			><path
-				d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
-			/></svg
-		>
-		<div class="w-full flex flex-col p-4">
-			<h2 class="text-2xl font-bold">P2PTrade</h2>
-			<h3 class="text-xl text-gray-500">
-				Gas efficent contracts for P2P swaps with ERC20, ERC721 and ERC1155 assets
-			</h3>
-		</div>
-	</a>
+
+
 </section>
